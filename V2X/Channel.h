79d76b05c48fd6e655248bd9c6949fedf03bffd7
Administrator <Administrator@XIUTINGXU>
2017-06-21 17:25:22 +0800

    添加聚合分片消息的时延处理
    修改了一些bug 添加了聚合分片消息的时延处理 main函数中写了一些测试用例 可以不用理会

--- V2X/Channel.h
+++ V2X/Channel.h
@@ -7,6 +7,6 @@ class Channel {
 public:
 	Channel() {};
 	virtual void bitSimulation(SliceMessage &sMsg) = 0;
-	virtual void delaySimulation(SliceMessage &sMsg, int index, std::list<CommunicationNode> communicationNodes) = 0;
+	virtual void delaySimulation(SliceMessage &sMsg, int index, std::list<CommunicationNode> communicationNodes, int simulation_count) = 0;
 
 };
